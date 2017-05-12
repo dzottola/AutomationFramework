09d3a8ad6d699948229bcb2cd5446a010cf8eb52
dzottola <dzottola>
2017-05-12 09:47:28 -0300

    firsttestupdate

--- src/com/test/TestWebElement2.java
+++ src/com/test/TestWebElement2.java
@@ -23,19 +23,11 @@ public class TestWebElement2 extends StartWebDriver{
 			driver.findElement(By.className("email")).sendKeys("diego.zottola@gpcloud.com");
 			driver.findElement(By.cssSelector("input[name=\"password\"]")).clear();
 		    driver.findElement(By.cssSelector("input[name=\"password\"]")).sendKeys("Enfermo@1");
-		    driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-
 		    driver.findElement(By.xpath("//div[@id='main']/div[2]/div/form/div[4]/button")).click();
-		    System.out.println("asdfasdddddddddddddddddddddddddddd");
 		    java.lang.Thread.sleep(10000);
-		    System.out.println("asdfasdddddddddddddddddddddddddddd");
-
 		    driver.findElement(By.xpath("//*[@id='nav']/li[5]/a")).click();
 		    driver.findElement(By.xpath("//ul[@id='nav']/li[5]/ul/li[2]/a")).click();
-		    driver.findElement(By.xpath("//input[@name='MerchantTransactionID']")).sendKeys("201751121467");
-		    
-		  
-		    
+		    java.lang.Thread.sleep(10000);
 
 		} catch (NoSuchElementException e) {
 			e.printStackTrace();
